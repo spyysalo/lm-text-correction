936e39650e2be1d7d9b6572b1cf8e4cd1e370555
Sampo Pyysalo <sampo.pyysalo@utu.fi>
2023-03-28 12:10:30 +0300

    Reorganize

--- train_seq2seq.py
+++ train_seq2seq.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
 
+# Train sequence-to-sequence model for error correction
+
 import sys
 import json
 import os
 
-import numpy as np
-import datasets
-import evaluate
-
-from statistics import mean
 from argparse import ArgumentParser
 from transformers import (
     AutoTokenizer,
@@ -17,6 +14,8 @@ from transformers import (
     Seq2SeqTrainingArguments,
     Seq2SeqTrainer,
 )
+
+from common import load_data, compute_metrics, compute_metrics_for_texts
 
 
 # Avoid "huggingface/tokenizers: The current process just got forked" warning
@@ -37,26 +36,6 @@ def argparser():
     return ap
 
 
-def load_data(fn, seed=42):
-    dataset = datasets.load_dataset('json', data_files=fn)
-
-    split = dataset['train'].train_test_split(
-        test_size=1000,
-        shuffle=False,    # in case data is ordered split sentences
-        seed=seed,
-    )
-
-    # rename
-    dataset = datasets.DatasetDict({
-        'train': split['train'],
-        'validation': split['test'],
-    })
-    
-    dataset = dataset.shuffle(seed=seed)
-    
-    return dataset
-
-
 def preprocess(data, tokenizer):
     input_texts = data['input']
     output_texts = data['output']
@@ -73,40 +52,6 @@ def preprocess(data, tokenizer):
 
     inputs['labels'] = outputs['input_ids']
     return inputs
-
-
-def compute_metrics_for_texts(predictions, references):
-    char_metric = evaluate.load('character')
-    word_metric = evaluate.load('wer')
-    
-    print(predictions[:10])
-    print(references[:10])
-
-    char_result = char_metric.compute(
-        predictions=predictions,
-        references=references
-    )
-
-    word_result = word_metric.compute(
-        predictions=predictions,
-        references=references
-    )
-    
-    return {
-        'cer_score': char_result['cer_score'],
-        'wer': word_result,
-    }
-
-    
-def compute_metrics(preds_and_refs, tokenizer):
-    pred_ids, ref_ids = preds_and_refs
-    
-    # -100 can't be decoded, so replace with pad id
-    ref_ids = np.where(ref_ids != -100, ref_ids, tokenizer.pad_token_id)
-    preds = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
-    refs = tokenizer.batch_decode(ref_ids, skip_special_tokens=True)
-
-    return compute_metrics_for_texts(preds, refs)
 
 
 def main(argv):
